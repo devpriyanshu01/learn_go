2423ba6f199e2904b8398c119f754f4acf24499f
RAB_5 <priyanshu00268@gmail.com>
2025-04-04 19:52:29 +0530

    till variables

---
+++ 02variables/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	var username string = "raman"
+	fmt.Println(username)
+	fmt.Printf("Type of username is: %T \n", username)
+
+	var isLazy bool = true
+	fmt.Println(isLazy)
+	fmt.Printf("Type of username is: %T \n", isLazy)
+
+}
