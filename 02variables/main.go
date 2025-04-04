4ba77962463ddc9287813c9243ef408584f07f68
RAB_5 <priyanshu00268@gmail.com>
2025-04-04 21:03:03 +0530

    few other variables

--- 02variables/main.go
+++ 02variables/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+const Token string = "ajsoeurasldjf"  //public as started with capital letter.
+
 func main(){
 	var username string = "raman"
 	fmt.Println(username)
@@ -10,5 +12,49 @@ func main(){
 	var isLazy bool = true
 	fmt.Println(isLazy)
 	fmt.Printf("Type of username is: %T \n", isLazy)
+
+	var marks int = 32
+	fmt.Println(marks)
+	fmt.Printf("Type of username is: %T \n", marks)
+
+	var decimal float64 = 22.23423424937429742
+	fmt.Println(decimal)
+	fmt.Printf("Type of username is: %T \n", decimal)
+
+	//default values
+	var defaultValue int
+	fmt.Println(defaultValue)
+	fmt.Printf("Type of username is: %T \n", defaultValue)
+
+	var defaultValue_float float32
+	fmt.Println(defaultValue_float)
+	fmt.Printf("Type of username is: %T \n", defaultValue_float)
+
+	var defaultValue_bool bool
+	fmt.Println(defaultValue_bool)
+	fmt.Printf("Type of username is: %T \n", defaultValue_bool)
+
+	fmt.Println("++++++++++++++++++++++++++++++++++++++++++++++++")
+	fmt.Println(Token)
+	fmt.Printf("Type of Token is : %T \n", Token)
+
+	fmt.Println("++++++++++++++++++++++++++++++++++++++++++++++++")
+
+	fmt.Println("Implicit variable declarations")
+	//implicit type
+	var website = "priyanshudev.in"
+	fmt.Println(website)
+
+	var mobile_no = 92342932784
+	fmt.Println(mobile_no)
+
+	
+	//no var style
+	fmt.Println("No var keyword declarations")
+	pincode := 829323
+	fmt.Println(pincode)
+
+	name := "Krsna"
+	fmt.Println(name)
 
 }
