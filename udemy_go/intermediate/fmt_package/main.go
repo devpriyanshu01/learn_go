eef0975244b6c230e114c511ff8159a9c9ab5444
RAB_5 <priyanshu00268@gmail.com>
2025-04-19 11:19:25 +0530

    error formatting functins

--- udemy_go/intermediate/fmt_package/main.go
+++ udemy_go/intermediate/fmt_package/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	//PRINTING ACTIONS IN GO
@@ -10,7 +12,7 @@ func main() {
 	fmt.Println("World")
 	fmt.Println(12, 456)
 
-	fmt.Print("Hello")	//no newline
+	fmt.Print("Hello") //no newline
 	fmt.Print("World")
 	fmt.Print(12, 456)
 
@@ -25,7 +27,7 @@ func main() {
 	//Foramtting Functions
 	fmt.Println(".............. Formatting Functions ..............")
 
-	s := fmt.Sprint("Hello", "World!", 1234, 1234325) 
+	s := fmt.Sprint("Hello", "World!", 1234, 1234325)
 	//formats but no space in between & no new line after the end.
 	fmt.Print(s)
 	fmt.Print("\n")
@@ -37,4 +39,32 @@ func main() {
 	sf := fmt.Sprintf("Name : %s, Age : %d", "Raman", 20)
 	fmt.Print(sf, "\n")
 
+	fmt.Println("---------- Scanning Functions ------------")
+
+	var userName string
+	var userAge int
+
+	fmt.Println("Enter name & age")
+	//accept input/scan
+	// fmt.Scan(&userName, &userAge)
+	// fmt.Scanln(&userName, &userAge)
+	// fmt.Scanf("%s %d", &userName, &userAge)
+
+	//print the result
+	fmt.Println("Name :- ", userName, "\nAge :- ", userAge)
+
+	//Error Formatting Functions
+	err := checkAge(122)
+	if err != nil {
+		fmt.Println("Error - ", err)
+	} else {
+		fmt.Println("Licensed To Drive")
+	}
+
+}
+func checkAge(age int) error {
+	if age < 18 {
+		return fmt.Errorf("age %d is too young to drive", age)
+	}
+	return nil
 }
