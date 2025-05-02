0a7be48d3283dd90ef665d43f4b84ec17b112bcc
RAB_5 <priyanshu00268@gmail.com>
2025-05-02 16:06:56 +0530

    read files in golang

---
+++ udemy_go/intermediate/reading_files/read_files.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("----- Reading Files in Golang ------")
+
+	// content, err := os.ReadFile("output.txt")
+	// if err != nil {
+	// 	fmt.Println("error reading file:", err)
+	// 	return
+	// }else {
+	// 	fmt.Println("file content:", content)
+	// }
+
+	//Open a file.
+	file, err := os.Open("output.txt")
+	if err != nil {
+		fmt.Println("error reading file:", err)
+		return
+	}else {
+		fmt.Println("File opened successfully!")
+	}
+	
+	defer func() {
+		fmt.Println("Closing the file")
+		file.Close()
+	}()
+
+	// //read contents of the open file at once.
+	// data := make([]byte, 1024)	//buffer to read the file
+	// _, err = file.Read(data)
+	// if err != nil {
+	// 	fmt.Println("error reading file:", err)
+	// 	return
+	// }
+	
+	// fmt.Println("content of the file:", string(data))
+
+	//Read a file line by line.
+	scanner := bufio.NewScanner(file)
+
+	//read line by line
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println("Line:", line)
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		fmt.Println("err reading file line by line:", err)
+		return
+	}
+
+}
