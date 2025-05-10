298e38b995173c3e0b3e2f8ceb90e3a7a4d40909
RAB_5 <priyanshu00268@gmail.com>
2025-05-10 08:58:30 +0530

    ubuffered channel behaviors

--- udemy_go/advanced/unbuffered_channel_lec105/unbuffered_channels.go
+++ udemy_go/advanced/unbuffered_channel_lec105/unbuffered_channels.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"time"
+	_"time"
 )
 
 func main() {
@@ -12,29 +12,45 @@ func main() {
 	//Unbuffered Channels block on receive if there is no corresponding send is ready.
 	ch := make(chan int)
 
-	go func() {
-		time.Sleep(2*time.Second)
-		ch <- 1
-	}()
+	// go func() {
+	// 	time.Sleep(2*time.Second)
+	// 	ch <- 1
+	// }()
 
-	go func(){
-		ch <- 2
-		time.Sleep(4*time.Second)
-	}()
+	// go func(){
+	// 	ch <- 2
+	// 	time.Sleep(4*time.Second)
+	// }()
 
-	receiver := <- ch
-	fmt.Println("received:", receiver)
+	// receiver := <- ch
+	// fmt.Println("received:", receiver)
 
-	receiver = <- ch
-	fmt.Println("received:", receiver)
+	// receiver = <- ch
+	// fmt.Println("received:", receiver)
 
 	//Unbuffered Channels block on send if there is no corresponding receiver ready.
 	//
-	go func(){
-		time.Sleep(5 * time.Second)
-		receiver1 := <- ch
-		fmt.Println("5 second goroutine finished.", receiver1)
-	}()
+	// go func(){
+	// 	time.Sleep(5 * time.Second)
+	// 	receiver1 := <- ch
+	// 	fmt.Println("5 second goroutine finished.", receiver1)
+	// }()
 	
-	ch <- 3
+	// ch <- 3
+
+	//Receiver awaits for all the goroutines to finish.
+	// go func(){
+	// 	time.Sleep(2*time.Second)
+	// 	fmt.Println("waited 2 seconds")
+	// }()
+	// receiver2 := <-ch
+	// fmt.Println("end of program", receiver2)
+
+	//Unbuffered Channels block on receive if there is no corresponding send is ready.
+	go func(){
+		// time.Sleep(2*time.Second)
+		// fmt.Println("inside goroutine")
+	}()
+	receiver2 := <-ch
+	fmt.Println("end of program", receiver2)
 }
