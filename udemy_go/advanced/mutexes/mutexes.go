2fdca1588489b00931ad3cf7e339513665d582b9
RAB_5 <priyanshu00268@gmail.com>
2025-05-18 13:27:27 +0530

    atomic counters

--- udemy_go/advanced/mutexes/mutexes.go
+++ udemy_go/advanced/mutexes/mutexes.go
@@ -31,41 +31,41 @@ func main() {
 	fmt.Println("Updated Value of count variable is:", count)
 }
 
-// type counter struct {
-// 	mu    sync.Mutex
-// 	count int
-// }
+type counter struct {
+	mu    sync.Mutex
+	count int
+}
 
-// func (c *counter) increment() {
-// 	c.mu.Lock()
-// 	defer c.mu.Unlock()
-// 	c.count++
-// }
+func (c *counter) increment() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.count++
+}
 
-// func (c *counter) getValue() int {
-// 	c.mu.Lock()
-// 	defer c.mu.Unlock()
-// 	return c.count
-// }
-// //So what we see is a program that creates a counter
-// //that multiple go routines can safely increment without
-// //interfering with each other.
-// func main() {
-// 	var wg sync.WaitGroup
-// 	counter := &counter{}
-// 	numGoroutines := 10
-// 	// wg.Add(numGoroutines)
+func (c *counter) getValue() int {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.count
+}
+//So what we see is a program that creates a counter
+//that multiple go routines can safely increment without
+//interfering with each other.
+func main() {
+	var wg sync.WaitGroup
+	counter := &counter{}
+	numGoroutines := 10
+	// wg.Add(numGoroutines)
 
-// 	for range numGoroutines {
-// 		wg.Add(1)
-// 		go func() {
-// 			defer wg.Done()
-// 			for range 1000 {
-// 				counter.increment()
-// 				// counter.count++
-// 			}
-// 		}()
-// 	}
-// 	wg.Wait()
-// 	fmt.Printf("Final counter value:%d\n", counter.getValue())
-// }
+	for range numGoroutines {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for range 1000 {
+				counter.increment()
+				// counter.count++
+			}
+		}()
+	}
+	wg.Wait()
+	fmt.Printf("Final counter value:%d\n", counter.getValue())
+}
