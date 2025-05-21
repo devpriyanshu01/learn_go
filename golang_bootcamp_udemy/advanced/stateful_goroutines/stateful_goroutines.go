fd46272099600e3ef74a3e07eaef801299eab1a7
RAB_5 <priyanshu00268@gmail.com>
2025-05-21 16:00:33 +0530

    stateful goroutines

---
+++ golang_bootcamp_udemy/advanced/stateful_goroutines/stateful_goroutines.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type statefulWorker struct {
+	count int
+	ch    chan int
+}
+
+func StartRoutine(sw *statefulWorker) {
+	go func() {
+		for {
+			select {
+			case receiver := <-sw.ch:
+				sw.count += receiver
+				fmt.Println("Update Value of Count:", sw.count)
+			default:
+			}
+		}
+	}()
+}
+
+
+func main() {
+	workerInstance := &statefulWorker{
+		count: 0,
+		ch : make(chan int),
+	}
+	StartRoutine(workerInstance)
+
+	for i := range 5 {
+		workerInstance.ch <- i
+		time.Sleep(time.Millisecond*200)
+	}
+
+}
