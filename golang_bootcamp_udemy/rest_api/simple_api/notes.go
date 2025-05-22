0161f445637c19acbac5dfb7e57e3e2268aa1df9
RAB_5 <priyanshu00268@gmail.com>
2025-05-22 18:26:29 +0530

    understanding modules and package in go

---
+++ golang_bootcamp_udemy/rest_api/simple_api/notes.go
@@ -0,0 +1,23 @@
+package main
+
+func main(){
+// Packages & Modules in Go
+
+// Every Go file belongs to a package & each package can be imported by other package.
+
+// Package are usually defined by a directory.
+
+// A module is a collection of related packages. A module is greater.
+
+// A module is a large larger unit of code organization & typically used to manage and version entire project or group of related package. 
+
+// A package must be a part of a module.
+
+// ### Commands
+
+// 1. go mod init <project_name> - initialize a package
+// 2. go get <path to package> - install any package
+// 3. go mod tidy - removes installed package which are not required
+// 4. go run 
+// 5. go build
+}
