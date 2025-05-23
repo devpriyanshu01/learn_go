6926b63e7fedbdbaba5b03a78d55275cb8d0dcf3
RAB_5 <priyanshu00268@gmail.com>
2025-05-23 14:38:17 +0530

    creating http2 server & making the server https but self hosted

--- golang_bootcamp_udemy/rest_api/simple_api/go.mod
+++ golang_bootcamp_udemy/rest_api/simple_api/go.mod
@@ -2,7 +2,6 @@ module simpleapi
 
 go 1.24.2
 
-require (
-	golang.org/x/net v0.40.0 // indirect
-	golang.org/x/text v0.25.0 // indirect
-)
+require golang.org/x/net v0.40.0
+
+require golang.org/x/text v0.25.0 // indirect
--- golang_bootcamp_udemy/rest_api/simple_api/server.go
+++ golang_bootcamp_udemy/rest_api/simple_api/server.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
+	"log"
 	"net/http"
+
+	"golang.org/x/net/http2"
 )
 
 func main(){
@@ -15,6 +19,45 @@ func main(){
 	})
 
 	port := 3000
+
+	//load the TLS certificate and key
+	cert := "cert.pem"
+	key := "key.pem"
+
+	//configure TLS
+	tlsConfig := &tls.Config{
+		MinVersion: tls.VersionTLS12,
+	}
+
+	//create a custom server
+	server := &http.Server{
+		Addr : fmt.Sprintf(":%d", port),
+		Handler: nil,
+		TLSConfig: tlsConfig,
+	}
+
+	//enable http2
+	http2.ConfigureServer(server, &http2.Server{})
+
 	fmt.Printf("Server is running on port: %d\n", port)
-	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+
+	err := server.ListenAndServeTLS(cert, key)
+	if err != nil {
+		log.Fatalln("error stating the server", err)
+	}
+
+	// err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	// if err != nil {
+	// 	log.Fatalln("error stating the server")
+	// }
+	//Generating self signed certificates on our local computer using openssl:
+	//openssl req -x509 -newkey rsa:2048 -nodes -keyout key.pem -out cert.pem -days 365
+	//PEM - Privacy Enhanced Mail
+	
+	//key.pem = private key & cert.pem = public key
+	//Generating these two keys separately.
+	//First generate private key
+	//openssl genpkey -algorithm RSA -out server.key -pkeyopt rsa_keygen_bits:2048
+	//Now generate public key
+	//openssl req -new -x509 -key server.key -out server.cert -days 365
 }
